5b5e1d086f2256b2c0081f60b279af50b921b2f2
Valdemir Pinheiro <pinheiroone@gmail.com>
2021-02-18 11:48:27 -0300

    DbContext Migration

---
+++ CursoMVC/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CursoMVC.Models
+{
+    public class Context : DbContext
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=localhost;Database=CursoMvc;user=sa;password=1234567");
+        }
+    }
+}
--- CursoMVC/Startup.cs
+++ CursoMVC/Startup.cs
@@ -1,3 +1,4 @@
+using CursoMVC.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -20,13 +21,14 @@ namespace CursoMVC
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // Injeções de dependência
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<Context>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // responsável pelos middleways
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
