ec190bfcd490cf474039bc0768de66115f84e904
Valdemir Pinheiro <pinheiroone@gmail.com>
2021-02-19 17:40:35 -0300

    improving the application

--- CursoMVC/Models/Categoria.cs
+++ CursoMVC/Models/Categoria.cs
@@ -1,8 +1,13 @@
-﻿namespace CursoMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CursoMVC.Models
 {
     public class Categoria
     {
         public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
         public string Descricao { get; set; }
     }
 }
---
+++ CursoMVC/Models/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "Campo Obrigatório")]
+        public string Descricao { get; set; }
+
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+    }
+}
